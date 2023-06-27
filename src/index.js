2746acae60755c8aec6058f160678f431a3914cf
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-06-26 18:22:53 -0600

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/extensions, no-unused-vars
 import { createLeaderboard } from './leaderboard';
 
 // use createLeaderboard function
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,4 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export function createLeaderboard() {
-    // implementation
-  }
-  
+  // implementation
+}

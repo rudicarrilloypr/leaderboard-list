af8af37f18ce56a3351f26eabb9b7408cbd38b8d
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-06-27 16:34:46 -0600

    Add functions and API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-import './style.css'; // Asegúrate de que la ruta a tu archivo CSS sea correcta
-// eslint-disable-next-line no-unused-vars
-import { createLeaderboard } from './leaderboard.js';
+import './style.css';
+import { createGame, getScores, postScore } from './api.js';
 
-// Use la función createLeaderboard aquí
+const refreshScores = async (gameId) => {
+  const scores = await getScores(gameId);
+  const scoreTable = document.querySelector('#scoreTable');
+  // clear existing scores
+  while (scoreTable.childElementCount > 1) {
+    scoreTable.lastChild.remove();
+  }
+  scores.forEach((score) => {
+    const scoreRow = document.createElement('tr');
+    scoreRow.innerHTML = `<td>${score.user}</td><td>${score.score}</td>`;
+    scoreTable.appendChild(scoreRow);
+  });
+};
+
+const submitScore = async (gameId) => {
+  const nameInput = document.querySelector('#nameInput');
+  const scoreInput = document.querySelector('#scoreInput');
+  const success = await postScore(gameId, nameInput.value, Number(scoreInput.value));
+  if (success) {
+    // clear inputs
+    nameInput.value = '';
+    scoreInput.value = '';
+  }
+  refreshScores(gameId);
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const newGame = await createGame('My Cool Game');
+  const [, , , gameId] = newGame.result.split(' ');
+  const refreshButton = document.querySelector('.btn');
+  const submitButton = document.querySelector('#submitButton');
+  refreshButton.addEventListener('click', () => refreshScores(gameId));
+  submitButton.addEventListener('click', () => submitScore(gameId));
+});
